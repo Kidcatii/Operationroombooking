9fd84b979b71e624411c9cc998ffb7b517b40dc5
Admin <popliveone@hotmail.com>
2020-09-05 19:25:12 +0700

    make it better

--- resources/js/pages/home.vue
+++ resources/js/pages/home.vue
@@ -67,8 +67,7 @@
 import axios from 'axios';
 window.moment = require('moment');
 import { mapGetters } from 'vuex'
-import booktable from './homecomponents/approvetable.vue'
-
+import booktable from './homecomponent/approvetable'
 export default {
   middleware: 'auth',
 
@@ -124,6 +123,8 @@ data(){
       },
 
     mounted(){
+        //this.getAlldata();
+        this.getBuildingData();
         this.myprop();
         this.runCron();
         this.groupdata();
@@ -138,7 +139,14 @@ data(){
           this.$router.push('bookingdetail/'+id)
         },
 
+        getAlldata(){
+          this.getBookingData();
+          this.getBuildingData();
+          this.getRoomData();
+          this.getUserData();
+          this.getJoingroupData();
 
+        },
 
         getBuildingData(){
             /*axios.get('api/bookings').then(response=>{
@@ -167,6 +175,36 @@ data(){
 
         },
 
+        getBuildingID(){
+            axios.get('api/buildings/'+1).then(response=>{
+                this.buildingsid = response.data;
+            })
+        },
+
+        getRoomData(){
+            axios.get('/api/rooms').then(response=>{
+              this.rooms = response.data.room;
+            })
+        },
+
+        getUserData(){
+          axios.get('/api/users').then(response=>{
+              this.users = response.data;
+            })
+        },
+
+        getJoingroupData(){
+          axios.get('/api/joingroups').then(response=>{
+              this.groupjoin = response.data;
+            });
+        },
+
+        getBuildingID(){
+            axios.get('api/buildings/'+1).then(response=>{
+                this.buildingsid = response.data;
+            })
+        },
+
         getBookingData(){
           axios.get('api/bookings').then(response=>{
             this.bookings = response.data;
@@ -184,6 +222,74 @@ data(){
           }*/
         },
 
+        Member(bookingid){
+          this.groupjoin.forEach((join)=>{
+            if (join.booking_id == bookingid){
+              this.member.push({
+                book:join.booking_id,
+                user:join.user_id
+                })
+            }
+          })
+         // debugger;
+        },
+
+        groupdata(){
+          //console.log(this.booking)
+          console.log("check1")
+          this.bookings.forEach((book)=>{
+            
+            console.log(book.id);
+          })
+
+          /*axios.get('/api/bookings').then(response=>{
+            this.bookings = response.data;
+            for (let i = 0; i < this.bookings.length; i++) {
+            axios.get('/api/rooms').then(response=>{
+              this.roomroom =response.data.room;
+              for (let j = 0; j < this.roomroom.length; j++) {
+                if(this.bookings[i].room_id == this.roomroom[j].id && this.bookings[i].status == 1){
+                  
+
+                  let start = moment(this.bookings[i].start,).format('YYYY-MM-DD HH:mm');
+                  let end = moment(this.bookings[i].end,).format('YYYY-MM-DD HH:mm');
+                  this.Member(this.bookings[i].id);
+                  this.membercount = this.member.length;
+
+                  this.tabb.push({
+                    id:this.bookings[i].id,
+                    start:start,
+                    end:end,
+                    roomname:this.roomroom[j].room_name,
+                    describe:this.bookings[i].describe,*/
+                    //person:this.num,
+                   /* member:this.membercount,
+                    count:this.bookings[i].count,
+                    password:this.bookings[i].password,
+                    update:new Date(this.bookings[i].updated_at)
+                  });
+
+                  this.member = []*/
+
+                  //this.member = []
+                  
+                  /*this.tabb.sort(function (a, b) {
+                      this.tabbb = a.update.localeCompare(b.update);
+                  });*/
+                  //this.tabbbb = this.tabb.sort((a, b) => b.update - a.update)
+                  //console.log(tabb);
+                  //this.tabb.reverse();
+                  //this.tabbbb = this.tabbb.slice(0,5)
+              //  }
+             // }
+            //})
+            
+          //}
+          
+          //})
+
+        },
+
         runCron(){
           var CronJob = require('cron').CronJob;
           var job = new CronJob('* 10 * * * *', function() {
@@ -193,25 +299,33 @@ data(){
             for(let index = 0; index < this.booking.length;index++ ) {
               
               if(this.booking[index].status === 0){
-
+                //console.log(this.booking[index].id);
                 var bookingdate = new Date(this.booking[index].created_at+' UTC');
                 
                 bookingdate.setMinutes(bookingdate.getMinutes()+10);
+                //console.log(timenow);
+                //console.log(bookingdate);
 
                 
                   if(timenow > bookingdate){
                     axios.put('/api/bookings/'+this.booking[index].id,{
                       status:2
                     });
-
+                    console.log('Cancel');
                     
                   }
 
-
+                  //if(timenow ===)
+                
+                //console.log(bookingdate);
+                //console.log(bookingdatesss);
+                //console.log(bookinghour);
+                //console.log(bookingminute);
+                //console.log(bookingsecond);
                 
               }
               else {
-
+                console.log('Checking Book status.');
               }
               }
             });
