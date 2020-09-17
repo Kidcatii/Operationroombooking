25e714a2e1589e7efd042ea418cfbba61c2af00b
Admin <popliveone@hotmail.com>
2020-09-18 03:37:00 +0700

    make it better

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -18,7 +18,7 @@ $config = [
        
 
   <title>{{ config('app.name') }}</title>
-  <!--<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">-->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
   <!--<link href="https://fonts.googleapis.com/css?family=Roboto:100,300,400,500,700,900" rel="stylesheet">-->
   <link href="https://cdn.jsdelivr.net/npm/@mdi/font@5.x/css/materialdesignicons.min.css" rel="stylesheet">
   <link href="https://cdn.jsdelivr.net/npm/vuetify@2.x/dist/vuetify.min.css" rel="stylesheet">
